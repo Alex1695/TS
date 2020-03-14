bc387765379e80eba396b81b14d3a320d188230b
Alejandro <a.amarillascamara@hotmail.com>
2020-03-14 18:20:41 +0100

    BBDD connection

---
+++ transportsoftware/src/main/java/com/ts/app/backend/BBDD_Conection.java
@@ -0,0 +1,43 @@
+package com.ts.app.backend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+public class BBDD_Conection {
+	private static Connection conex = null;
+
+	private String url ="jdbc:mysql://192.168.1.95:32787/DES_TS";
+	//private String url ="jdbc:mysql://sierralfa.synology.me:32787/DES_TS";
+	private String user = "PGPI20";
+	private String pass = "paSSw0rd";
+
+	/**
+	 * Constructor privado para implementar singleton
+	 * @return
+	 */
+	private Connection conexion() {	
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conex = (Connection) DriverManager.getConnection(url, user, pass);
+			System.out.println("Conexión establecida con BBDD");
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("ERROR al establecer la conexión BBDD!");
+		}
+		return conex;
+	}
+	/**
+	 * Singleton de la conexion
+	 * @return
+	 */
+	public static Connection getConexionInstance() {
+		if(conex !=null) {
+			return conex;
+		}else {
+			conex = new BBDD_Conection().conexion();
+			return conex;
+		}
+	}
+}
---
+++ transportsoftware/src/main/resources/BBDD/inserts.sql
@@ -0,0 +1,5 @@
+
+-- Truck types and load/download times
+INSERT INTO `DES_TS`.`TB_truckType` (`truckType`, `load`, `download`) VALUES ('FURGONETA', '20', '15');
+INSERT INTO `DES_TS`.`TB_truckType` (`truckType`, `load`, `download`) VALUES ('LONA', '40', '30');
+INSERT INTO `DES_TS`.`TB_truckType` (`truckType`, `load`, `download`) VALUES ('TRAILER', '60', '45');
