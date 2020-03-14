82e4641caf650d665eb9c816e22de930f521b0cd
Jorge <jorge.delafuente95@outlook.com>
2020-03-15 00:01:05 +0100

    Change in booking view

--- transportsoftware/frontend/src/views/reserva/reserva-view.js
+++ transportsoftware/frontend/src/views/reserva/reserva-view.js
@@ -2,17 +2,22 @@ import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import '@vaadin/vaadin-form-layout/vaadin-form-layout.js';
 import '@vaadin/vaadin-form-layout/vaadin-form-item.js';
-import '@vaadin/vaadin-text-field/vaadin-text-field.js';
-import '@vaadin/vaadin-text-field/vaadin-text-area.js';
 import '@vaadin/vaadin-button/vaadin-button.js';
 import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout.js';
 import '@vaadin/vaadin-ordered-layout/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-checkbox/src/vaadin-checkbox.js';
+import '@vaadin/vaadin-date-picker/src/vaadin-date-picker.js';
+import '@vaadin/vaadin-time-picker/src/vaadin-time-picker.js';
+import '@vaadin/vaadin-text-field/vaadin-text-field.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-form-layout/src/vaadin-form-item.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 class ReservaView extends PolymerElement {
   static get template() {
     return html`
-      <custom-style>
-        <style include="shared-styles lumo-typography">
+<custom-style>
+ <style include="shared-styles lumo-typography">
           #reserva-view {
               display: block;
               padding: 1rem;
@@ -30,39 +35,72 @@ class ReservaView extends PolymerElement {
               justify-content: flex-end
           }
         </style>
-      </custom-style>
-
-      <vaadin-vertical-layout id="wrapper" theme="padding">
-       <h1>Form</h1>
-       <vaadin-form-layout>
-        <vaadin-form-item>
-         <label slot="label">First name</label>
-         <vaadin-text-field id="firstname" class="full-width" value=""></vaadin-text-field>
-        </vaadin-form-item>
-        <vaadin-form-item>
-         <label slot="label">Last name</label>
-         <vaadin-text-field id="lastname" class="full-width" value=""></vaadin-text-field>
-        </vaadin-form-item>
-        <vaadin-form-item colspan="2">
-         <label slot="label">Email</label>
-         <vaadin-text-field id="email" class="full-width" value=""></vaadin-text-field>
-        </vaadin-form-item>
-        <br>
-        <vaadin-form-item colspan="2">
-         <label slot="label">Notes</label>
-         <vaadin-text-area id="notes" class="full-width" colspan=""></vaadin-text-area>
-        </vaadin-form-item>
-       </vaadin-form-layout>
-       <vaadin-horizontal-layout theme="spacing" style="display:flex;flex-wrap:wrap-reverse;width:100%;justify-content:flex-end;">
-        <vaadin-button theme="tertiary" id="cancel" slot="">
-          Cancel
-        </vaadin-button>
-        <vaadin-button theme="primary" id="save">
-          Save
-        </vaadin-button>
-       </vaadin-horizontal-layout>
-      </vaadin-vertical-layout>
-    `;
+</custom-style>
+<vaadin-vertical-layout id="wrapper" theme="padding">
+ <h1>Reservar un muelle</h1>
+ <vaadin-form-layout style="align-self: center;">
+  <vaadin-form-item style="width: 100%;">
+   <label slot="label">¿Qué desea hacer?</label>
+   <vaadin-checkbox id="check_reservar">
+     Reservar muelle 
+   </vaadin-checkbox>
+   <vaadin-checkbox id="check_modificar">
+     Modificar reserva 
+   </vaadin-checkbox>
+  </vaadin-form-item>
+  <br>
+  <vaadin-form-item style="width: 450px;">
+   <label slot="label">Pedido</label>
+   <vaadin-text-field id="pedido" style="width: 300px;"></vaadin-text-field>
+  </vaadin-form-item>
+  <vaadin-form-item style="width: 420px;">
+   <label slot="label">Matrícula</label>
+   <vaadin-text-field id="matricula" class="full-width" value="" style="width: 300px;"></vaadin-text-field>
+  </vaadin-form-item>
+  <br>
+  <vaadin-form-item colspan="2">
+   <label slot="label">Acción</label>
+   <vaadin-checkbox id="check_descarga">
+     Descarga 
+   </vaadin-checkbox>
+   <vaadin-checkbox id="check_carga">
+     Carga 
+   </vaadin-checkbox>
+  </vaadin-form-item>
+  <vaadin-form-item colspan="2">
+   <label slot="label">Tipo de camión</label>
+   <vaadin-checkbox id="check_trailer">
+     Trailer 
+   </vaadin-checkbox>
+   <vaadin-checkbox id="check_furgoneta">
+     Furgoneta 
+   </vaadin-checkbox>
+   <vaadin-checkbox id="check_lona">
+     Lona 
+   </vaadin-checkbox>
+  </vaadin-form-item>
+  <vaadin-form-item style="width: 450px;">
+   <label slot="label">Fecha de reserva</label>
+   <vaadin-date-picker style="flex-grow: 0; width: 300px;" id="seleccion_dia"></vaadin-date-picker>
+  </vaadin-form-item>
+  <vaadin-form-item>
+   <label slot="label">Hora de reserva</label>
+   <vaadin-time-picker id="seleccion_hora" style="width: 300px;" max="14:00:00.00" min="06:00:00.00" value="06:00"></vaadin-time-picker>
+  </vaadin-form-item>
+ </vaadin-form-layout>
+ <vaadin-horizontal-layout style="display:flex; flex-wrap:wrap-reverse; width:100%; justify-content: flex-end;">
+  <vaadin-button theme="primary error" id="cancelar_reserva">
+    Cancelar reserva 
+  </vaadin-button>
+  <vaadin-button theme="tertiary" id="cancelar" slot="">
+    Vaciar selección 
+  </vaadin-button>
+  <vaadin-button theme="primary" id="reservar">
+    Save 
+  </vaadin-button>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   static get is() {
--- transportsoftware/src/main/java/com/ts/app/views/reserva/ReservaView.java
+++ transportsoftware/src/main/java/com/ts/app/views/reserva/ReservaView.java
@@ -1,18 +1,27 @@
 package com.ts.app.views.reserva;
 
 import com.ts.app.backend.Employee;
+import com.ts.app.backend.model.booking;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 
 import com.ts.app.MainView;
 import com.ts.app.views.reserva.ReservaView.ReservaViewModel;
@@ -31,31 +40,234 @@ public class ReservaView extends PolymerTemplate<ReservaViewModel> {
 
     }
 
-    @Id
-    private TextField firstname;
-    @Id
-    private TextField lastname;
-    @Id
-    private TextField email;
-    @Id
-    private TextArea notes;
-
-    @Id
-    private Button cancel;
-    @Id
-    private Button save;
+    @Id("check_trailer")
+	private Checkbox check_trailer;
+	@Id("check_furgoneta")
+	private Checkbox check_furgoneta;
+	@Id("check_lona")
+	private Checkbox check_lona;
+	@Id("pedido")
+	private TextField pedido;
+	@Id("matricula")
+	private TextField matricula;
+	@Id("check_descarga")
+	private Checkbox check_descarga;
+	@Id("check_carga")
+	private Checkbox check_carga;
+	@Id("reservar")
+	private Button reservar;
+	@Id("cancelar")
+	private Button cancelar;
+	@Id("seleccion_dia")
+	private DatePicker seleccion_dia;
+	@Id("seleccion_hora")
+	private TimePicker seleccion_hora;
+	@Id("check_reservar")
+	private Checkbox check_reservar;
+	@Id("check_modificar")
+	private Checkbox check_modificar;
+	@Id("cancelar_reserva")
+	private Button cancelar_reserva;
 
     public ReservaView() {
 
-        // Configure Form
-        Binder<Employee> binder = new Binder<>(Employee.class);
+    	reservar.setVisible(false);
+    	cancelar.setVisible(false);
+    	cancelar_reserva.setVisible(false);
+    	
+    	pedido.setClearButtonVisible(true);
+    	matricula.setClearButtonVisible(true);
+    	
+    	/////////////////////// RESERVAR/MODIFICACIÓN //////////////////////////////////////
+    	check_reservar.addValueChangeListener(e -> {
+    		if (check_reservar.getValue().equals(true)) {
+    			check_modificar.setValue(false);
+    			reservar.setText("Reservar");
+    			reservar.setVisible(true);
+    	    	cancelar.setVisible(true);
+    		}
+    		
+    		
+    		if (check_reservar.getValue().equals(false) && check_modificar.getValue().equals(false)) {
+    			reservar.setVisible(false);
+    	    	cancelar.setVisible(false);
+    	    	cancelar_reserva.setVisible(false);
+    		}
 
-        // Bind fields. This where you'd define e.g. validation rules
-        binder.bindInstanceFields(this);
+    	});
+    	
+    	check_modificar.addValueChangeListener(e -> {
+    		if (check_modificar.getValue().equals(true)) {
+    			check_reservar.setValue(false);
+    			reservar.setText("Modificar");
+    			reservar.setVisible(true);
+    	    	cancelar.setVisible(true);
+    	    	cancelar_reserva.setVisible(true);
+    		}
+    		
+    		if (check_reservar.getValue().equals(true)) {
+    			cancelar_reserva.setVisible(false);
+    		}
+    		
+    		if (check_reservar.getValue().equals(false) && check_modificar.getValue().equals(false)) {
+    			reservar.setVisible(false);
+    	    	cancelar.setVisible(false);
+    	    	cancelar_reserva.setVisible(false);
+    		}
+    	});
+    	
+    	
+    	////////////////////// ELECCIÓN DE CARGA/DESCARGA /////////////////////////////
+    	check_carga.addValueChangeListener(e -> {
+    		if (check_carga.getValue().equals(true)) {
+    			check_descarga.setValue(false);
+    		}
 
-        cancel.addClickListener(e -> binder.readBean(null));
-        save.addClickListener(e -> {
-            Notification.show("Not implemented");
-        });
+    	});
+    	
+    	check_descarga.addValueChangeListener(e -> {
+    		if (check_descarga.getValue().equals(true)) {
+    			check_carga.setValue(false);
+    		}
+    	});
+    	
+    	/////////////////////////////////// ELECCIÓN TIPO CAMIÓN ////////////////////////
+    	check_trailer.addValueChangeListener(e -> {
+    		if(check_trailer.getValue().equals(true)) {
+	    		check_furgoneta.setValue(false);
+	    		check_lona.setValue(false);
+    		}
+
+    	});
+    	
+    	check_furgoneta.addValueChangeListener(e -> {
+    		if(check_furgoneta.getValue().equals(true)) {
+	    		check_trailer.setValue(false);
+	    		check_lona.setValue(false);
+    		}
+
+    	});
+    	
+    	check_lona.addValueChangeListener(e -> {
+    		if(check_lona.getValue().equals(true)) {
+	    		check_furgoneta.setValue(false);
+	    		check_trailer.setValue(false);
+    		}
+
+    	});
+
+    	Label content = new Label("Reserva realizada!");
+    	Notification notification = new Notification(content);
+    	notification.setDuration(3000);
+    	notification.setPosition(Position.MIDDLE);
+    	
+    	Label content1 = new Label(
+    	        "Faltan datos o son incorrectos!");
+    	Notification notification1 = new Notification(content1);
+    	notification1.setDuration(3000);
+    	notification1.setPosition(Position.MIDDLE);
+    	
+    	/*LocalDateTime dia_reserva = LocalDateTime.now();
+    	seleccion_dia.setMin(dia_reserva.toLocalDate());
+    	LocalDateTime dias_siguientes = dia_reserva.plusDays(5);
+    	seleccion_dia.setMax(dias_siguientes.toLocalDate());
+    	
+    	Binder<booking> binder = new Binder<>();
+    	binder.forField(seleccion_dia).withValidator(
+   	        value -> !DayOfWeek.SATURDAY.equals(value.getDayOfWeek()) && !DayOfWeek.SUNDAY.equals(value.getDayOfWeek()),
+    	        "Debes elegir un día entre lunes y viernes").bind(booking::getDia_reservado, booking::setDia_reservado);
+    	*/
+    	cancelar.addClickListener(e -> {
+    		pedido.clear();
+    		matricula.clear();
+    		check_descarga.setValue(false);
+    		check_carga.setValue(false);
+    		check_furgoneta.setValue(false);
+    		check_lona.setValue(false);
+    		check_trailer.setValue(false);
+    		check_reservar.setValue(false);
+    		check_modificar.setValue(false);
+    		seleccion_dia.clear();
+    		seleccion_hora.clear();
+    	});
+    	
+    	
+    	// Imprimimos valores
+    	reservar.addClickListener( e -> {
+    		
+    		String valor_pedido = pedido.getValue();
+    		String accion = "";
+    		String tipo = "";
+    		String valor_matricula = matricula.getValue();
+    		String dia_reservado = seleccion_dia.getValue().toString();
+    		int validez_matricula = 0;
+    		int validez_pedido = 0;
+    		
+    		// Comprobamos si el valor del pedido es correcto
+    		if (valor_pedido.matches("^[0-9]{6}")) {
+    			System.out.println("Pedido valido");
+    			validez_pedido = 1;
+    		} else {
+    			System.out.println("Pedido invalido");
+    			validez_pedido = 0;
+    		}
+    		
+    		// Comprobamos si la matrícula tiene el formato correcto
+    		if (valor_matricula.toUpperCase().matches("^[0-9]{4}[A-Z]{3}$")) {
+		        System.out.println("Matrícula válida");
+		        validez_matricula = 1;
+		    }else{
+		    	validez_matricula = 0;
+		        System.out.println("Matrícula inválida");
+		    }   
+    		 
+    		// Cogemos los valores de carga/descarga y el tipo de camión
+    		if(check_descarga.getValue().equals(true)) {
+    			accion = "Descarga";	
+    		}
+    		
+    		if(check_carga.getValue().equals(true)) {
+    			accion = "Carga";	
+    		}
+    		
+    		if(check_trailer.getValue().equals(true)) {
+    			tipo = "Trailer";	
+    		}
+    		
+    		if(check_furgoneta.getValue().equals(true)) {
+    			tipo = "Furgoneta";	
+    		}
+    		
+    		if(check_lona.getValue().equals(true)) {
+    			tipo = "Lona";	
+    		}
+    		
+    		// Si todos los datos son correctos
+    		if (validez_matricula == 1 && validez_pedido == 1
+    				&& (check_descarga.getValue().equals(true) || check_carga.getValue().equals(true)) &&
+    				(check_furgoneta.getValue().equals(true) || check_trailer.getValue().equals(true) || 
+    						check_lona.getValue().equals(true) && check_reservar.getValue().equals(true) || check_modificar.getValue().equals(true))){
+    			notification.open();
+    			System.out.println("===================");
+    			System.out.println("Pedido: " + valor_pedido);
+        		System.out.println("Accion: " + accion);
+        		System.out.println("Tipo: " + tipo);
+        		System.out.println("Matricula: " + valor_matricula.toUpperCase());
+        		System.out.println("Dia reserva: " + dia_reservado);
+        		
+        		pedido.clear();
+        		matricula.clear();
+        		check_descarga.setValue(false);
+        		check_carga.setValue(false);
+        		check_furgoneta.setValue(false);
+        		check_lona.setValue(false);
+        		check_trailer.setValue(false);
+        		check_reservar.setValue(false);
+        		check_modificar.setValue(false);
+    		} else {
+    			notification1.open();
+    		}
+    		
+    	});
     }
 }
